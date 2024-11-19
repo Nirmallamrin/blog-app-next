9f43898424fa5254a3a9453e9d4b7f8390f2e341
[nirmal] <nirmallamrin77@gmail.com>
2024-11-19 19:41:25 +0530

    basic ui implemented

---
+++ app/about/page.js
@@ -0,0 +1,8 @@
+export default function About(){
+    return(
+        <main className="container mx-auto px-4 py-6">
+        <h2 className="text-4xl font-bold mb-4">About Us</h2>
+        <p>This is my first broject using Next js,for learning coding and building projects easily</p>
+     </main>
+    )
+}
---
+++ components/Footer.js
@@ -0,0 +1,10 @@
+export default function Footer(){
+    return(
+        <footer className="fixed inset-x-0 bottom-0 flex flex-col items-center bg-neutral-900 text-center text-white">
+            <div className="w-full p-4 text-center bg-slate-500">
+            © 2024 Nirmal
+            </div>
+
+        </footer>
+    )
+}
