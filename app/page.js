d96e4b3f8dc930d1635b04931bd17386c05ce291
[nirmal] <nirmallamrin77@gmail.com>
2024-11-23 14:39:18 +0530

    search funtionalities implemented

--- app/page.js
+++ app/page.js
@@ -1,18 +1,35 @@
 "use client"
 
 import Image from "next/image";
-import { useState, useEffect } from "react";
+import { useState, useEffect, useRef } from "react";
 import Link from "next/link";
 
 export default function Home() {
 
-  const [posts, setPosts] = useState([])
+  const [posts, setPosts] = useState([]);
+  const inputRef = useRef("");
+  const [search, setSearch] = useState(false)
 
   useEffect(()=>{
     fetch(process.env.NEXT_PUBLIC_API +"/posts")
     .then((res) =>res.json())
     .then((res) => setPosts(res))
   },[])
+
+  const searchPost = (e) => {
+    if(e.type == 'keydown' && e.key !== 'Enter'){
+      return;
+    }
+
+    setSearch(true);
+    // setTimeout(() => {
+      fetch(process.env.NEXT_PUBLIC_API +"/posts?q=" + inputRef.current.value)
+      .then((res) =>res.json())
+      .then((res) => setPosts(res))
+      .finally(() => setSearch(false))
+    // }, 1000)
+  
+  }
 
   return (
     <>   
@@ -23,8 +40,8 @@ export default function Home() {
       </p>
     </main>
         <div className="flex justify-end px-4">
-        <input type="text" className="px-4 py-2 border border-gray-300 rounded-md" placeholder="Search..." />
-        <button className="px-4 py-2 bg-blue-500 text-white rounded-md ml-4">Search</button>
+        <input onKeyDown={searchPost} ref={inputRef}  type="text" className="px-4 py-2 border border-gray-300 rounded-md" placeholder="Search..." />
+        <button onClick={searchPost} disabled={search} className="px-4 py-2 bg-blue-500 text-white rounded-md ml-4">{search?'...':'Search'}</button>
       </div>
       <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4">
         {posts.map((post) =>(
@@ -37,6 +54,7 @@ export default function Home() {
         </Link>
         ))
         }
+        {!posts.length > 0 && inputRef.current.value && <p>No Posts available for this query: <b>{inputRef.current.value}</b></p>}
       </div>
       </>
   );
